84d296cfa0930127090eb1e4b44c8e8a68ed06f8
GaneshSURESH <37071498+GaneshSURESH@users.noreply.github.com>
2018-03-20 17:31:18 +0530

    Create numeric digit.c

---
+++ numeric digit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include,conio.h>
+int main()
+{
+    long long n;
+    int count = 0;
+    clrscr();
+    printf("ENTER THE INTEGER: ");
+    scanf("%lld", &n);
+    while(n != 0)
+    {
+        // n = n/10
+        n /= 10;
+        ++count;
+    }
+    printf("Number of values: %d", count);
+}
