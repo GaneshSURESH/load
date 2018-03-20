51dbecb4a42be7c9ce3b7f2be262fad16cc763a1
GaneshSURESH <37071498+GaneshSURESH@users.noreply.github.com>
2018-03-20 17:48:46 +0530

    Create next.c

---
+++ next.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("enter the value of a");
+scanf("%d",&a)
+a=a+1;
+printf("after the process value");
+scanf("%d",&a);
+}
