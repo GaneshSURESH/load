7f02c6eb2b6a224b93f9265a2695c9b5653c1d84
GaneshSURESH <37071498+GaneshSURESH@users.noreply.github.com>
2018-05-24 14:40:44 +0530

    Create prgm82.c

---
+++ prgm82.c
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <iomanip>
+int main () 
+{
+int a=1.626,b=2.31;
+int area;
+area=a*b;
+  std::cout << std::setprecision(5) << f << '\n';
+  std::cout << std::setprecision(9) << f << '\n';
+  std::cout << std::fixed;
+  std::cout << std::setprecision(5) << f << '\n';
+  std::cout << std::setprecision(9) << f << '\n';
+  return 0;
+}
