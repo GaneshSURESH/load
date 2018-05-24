9c5ebf84640a66552f9389d12e30bfbb9890c17f
GaneshSURESH <37071498+GaneshSURESH@users.noreply.github.com>
2018-05-24 09:29:18 +0530

    Create between.cpp

---
+++ between.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+int a,b,c;
+cout<<"enter the value of a:";
+cin>>a;
+cout<<"enter the value of b:";
+cin>>b;
+cout<<"enter the value of c:";
+cin>>c;
+if(c<=a && c>=b)
+{
+cout<<"yes";
+}
+else
+{
+cout<<"no";
+}
+return 0;
+}
