0c1238d6ce6b7f84676a49cbd35771db33837edd
GaneshSURESH <37071498+GaneshSURESH@users.noreply.github.com>
2018-05-24 15:49:58 +0530

    Create prgm85.java

---
+++ prgm85.java
@@ -0,0 +1,32 @@
+import java.io.*;
+public static void main(String[] args)
+{
+    Scanner scan    =   new Scanner(System.in);
+    int a   =   scan.nextInt();
+    scan.nextLine();
+    for(int i=0 ; i<a ; i++)
+    {
+        String  myString    =   scan.nextLine();
+        int evn =   0,
+            odd =   0,
+            len =   myString.length();
+        char    strE[]  =   new char[50],
+                strO[]  =   new char[50];
+        for(int j=0 ; j<len ; j++)
+        {
+            if(j%2 == 0)
+            {
+                strE[evn]   =   myString.charAt(j);
+                evn++;
+            }
+            if(j%2 == 1)
+            {
+                strO[odd]   =   myString.charAt(j);
+                odd++;
+            }
+        }
+        System.out.print(strE);
+        System.out.print(" ");
+        System.out.println(strO);
+    }
+}
