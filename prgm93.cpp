a0e35cf9b7b07c2b69c61da08e54613131be85fb
GaneshSURESH <37071498+GaneshSURESH@users.noreply.github.com>
2018-05-25 09:40:25 +0530

    Create prgm93.cpp

---
+++ prgm93.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+float Celsius_to_Kelvin(float C)
+{
+    return (C + 273.15);
+}
+int main()
+{
+    float C = 100;
+    cout << "Temperature in Kelvin ( K ) = "<< Celsius_to_Kelvin(C);
+    return 0;
+}
