fd16838c6e0b2064060391b5b0dd97be05f914dc
GaneshSURESH <37071498+GaneshSURESH@users.noreply.github.com>
2018-05-25 09:56:31 +0530

    Create prgm94.c

---
+++ prgm94.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c;
+    Scanner s=new Scanner(System.in);
+    a=s.nextInt();
+    b=s.nextInt();
+    for(int i=1;i<=a;i++){
+    if(b==i)
+        {
+            printf(i);
+        }
+    }
+
+           
+   }
+}
