14c2fca0401bcd1ba3598c00bae96ac21de839b7
GaneshSURESH <37071498+GaneshSURESH@users.noreply.github.com>
2018-05-25 12:24:57 +0530

    Create prgm109.c

---
+++ prgm109.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,n,d;
+float sum;
+clrscr();
+printf("enter the values of a,n,d");
+scanf("%d%d%d",&a,&n,&d);
+sum=a+(a+(n-1)d);
+printf("sum of AP series is %f",sum);
+}
