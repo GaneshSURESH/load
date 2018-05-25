cab1e6f8e65bb1ee070e71236dcd666da5eb88ef
GaneshSURESH <37071498+GaneshSURESH@users.noreply.github.com>
2018-05-25 14:11:17 +0530

    Create prgm113.c

---
+++ prgm113.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+   int a[10],s,k,count=0,i;
+   clrscr();
+   scanf("%d %d",&s,&k);
+   for(i=0;i<s0;i++)
+   {
+   scanf("%d",&a[i]);
+   }
+   for(i=0;i<s;i++)
+   {
+       if(a[i]==k)
+       count++;
+   }
+   printf("%d",count);
+    return 0;
+}
