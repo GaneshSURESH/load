51a354e7705cb39bee556f5178e0ba06a190357e
GaneshSURESH <37071498+GaneshSURESH@users.noreply.github.com>
2018-05-25 12:32:54 +0530

    Create prgm110.c

---
+++ prgm110.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int b,a,sum;
+    clrscr();
+    printf("last number");
+    scanf("%d",&b);
+    for(a=0;a<=b;a++)
+    {
+        sum+=a;  
+    }
+    printf("%d",sum);
+    return 0;
